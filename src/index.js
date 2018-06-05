7569ebe3a0c7a7f29bab508059d170ae87fe44ae
dejan-krstic <dejanmkrstic@gmail.com>
2018-06-05 14:32:10 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import '../resources/style/main.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import App from './components/app';
+
+ReactDOM.render(
+        <App />,
+    document.querySelector('#app-container')
+);
+
