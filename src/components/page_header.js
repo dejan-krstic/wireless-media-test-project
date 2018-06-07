b751e9b0569da2657dbec9c895ffd73ce2224ed4
dejan-krstic <dejanmkrstic@gmail.com>
2018-06-07 15:01:27 +0200

    main page styling finished

--- src/components/page_header.js
+++ src/components/page_header.js
@@ -5,20 +5,21 @@ import tagTypesImage from "../../resources/assets/icons/32px/379-paragraph-justi
 
 export default () => $("#app-container").prepend(
                                               `<header class="row page-header">
-                                                <div class="col-sm-2"><div class="page-header__tags--image"><img src="${tagsImage}" alt="tags"></div><div> TAGS</div></div>
-                                                <div class="col-sm-7"></div>
-                                                <div class="col-sm-3 row">
-                                                  <div class="page-header__create-new col-sm-6" data-toggle="modal" data-target="#modal__create-new">
+                                                <div class="col-sm-4 page-header__tags"><div class="page-header__tags--image"><img src="${tagsImage}" alt="tags"></div><div> TAGS</div></div>
+                                                <div class="col-sm-3"></div>
+                                                <div class="col-sm-5 page-header__buttons">
+                                                <div class="page-header__justify" data-toggle="modal" data-target="#modal__create-new">
+                                                  <div class="page-header__justify--image">
+                                                    <div></div>
+                                                    <img src="${tagTypesImage}" alt="justify">
+                                                  </div>
+                                                  <div>Tag types</div>
+                                                </div>
+                                                  <div class="page-header__create-new" data-toggle="modal" data-target="#modal__create-new">
                                                     <div class="page-header__create-new--image">
                                                       <img src="${newImage}" alt="new">
                                                     </div>
                                                     <div>New</div>
-                                                  </div>
-                                                  <div class="page-header__justify col-sm-6" data-toggle="modal" data-target="#modal__create-new">
-                                                    <div class="page-header__justify--image">
-                                                      <img src="${tagTypesImage}" alt="justify">
-                                                    </div>
-                                                    <div>Tag types</div>
                                                   </div>
                                                 </div>
                                               </header>`
